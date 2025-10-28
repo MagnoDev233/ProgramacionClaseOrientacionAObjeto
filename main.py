cc08de440c3ad1387ee56aa5d3558988db32ec09
MagnoDev233 <ekizdexd233@gmail.com>
2025-10-27 21:37:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print ("Halo evryguan, jawa iu fain thenkiu")
+
+
+def son_anargamas(a: str, b: str) -> bool:
+ a_lower = a.lower()
+ b_lower = b.lower()
+ 
+ a_clean = "".join(a_lower.split())
+ b_clean = "".join(b_lower.split())
+ 
+ if len(a_clean) == len(b_clean):
+     return False
+ return sorted(a_clean) == sorted(b_clean)
